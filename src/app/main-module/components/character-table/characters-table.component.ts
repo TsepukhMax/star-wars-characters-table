1230dd3226723a1e7f98dc0d7b0ad243aec979fa
TsepukhMax <163754175+TsepukhMax@users.noreply.github.com>
2025-03-04 18:50:24 +0200

    finalize commit

--- src/app/main-module/components/character-table/characters-table.component.ts
+++ src/app/main-module/components/character-table/characters-table.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { IPerson, IPlanet } from '../../../interfaces';
 import { StarWarsService } from '../../../services/star-wars.service';
 
+const MIN_WINDOW_WIDTH = 1024;
+
 @Component({
   selector: 'app-characters-table',
   templateUrl: './characters-table.component.html',
@@ -12,17 +14,23 @@ export class CharactersTableComponent {
   selectedPlanet: IPlanet;
   topCoordinate: number = 0;
   @Input() people: IPerson[] = [];
+  selectedPerson: IPerson;
+
 
   constructor(private starWarsService: StarWarsService) {}
   
   requestPlanet(person: IPerson, event:Event): void {
     if(person.homeworldUrl !== this.selectedPlanet?.url){
       const coord = (event.currentTarget as HTMLElement)?.offsetTop;
+      const height = (event.currentTarget as HTMLElement)?.offsetHeight * 0.67;
       this.starWarsService.getPlanet(person.homeworldUrl).subscribe((planet) => {
+        const screenWidth = window.innerWidth;
+        this.topCoordinate = screenWidth < MIN_WINDOW_WIDTH ? coord + height : coord;
         this.selectedPlanet = planet;
-        this.topCoordinate = coord;
+        this.selectedPerson = person;
       })
     }
     this.selectedPlanet = null;
+    this.selectedPerson = null;
   }
 }
